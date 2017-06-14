d2d15e964b836985818946662ef91a5214679606
Non-Contradiction <lch34677@gmail.com>
2017-06-13 21:30:17 -0400

    Expr implementation, version 0.3.0

--- convexr/R/first.R
+++ convexr/R/first.R
@@ -1,6 +1,7 @@
 .convex <- new.env(parent = emptyenv())
 .convex$vars <- 0
 .convex$ps <- 0
+.convex$exprs <- 0
 
 .check_install <- function(pkgname){
     command <- paste0('if Pkg.installed("', pkgname, '") == nothing Pkg.add("', pkgname, '") end')
@@ -113,6 +114,32 @@ expr_text <- function(x, env){
         gsub(pattern = "/", replacement = "./") %>%
         gsub(pattern = "\\^", replacement = ".^") %>%
         gsub(pattern = "%.\\*%", replacement = "*")
+}
+
+#' Create expressions to be used for optimization problem creation
+#'
+#' \code{Expr} create expressions, which can be used later for problem creation.
+#'
+#' @param x expression to be created.
+#'
+#' @examples
+#' x <- Variable(2)
+#' x1 <- Expr(x + 1)
+#' @export
+Expr <- function(x){
+    .start()
+    .convex$exprs <- .convex$exprs + 1
+    expression_text <- expr_text(sys.call()[[2]], env = parent.frame())
+    ## print(ptext)
+    Jname <- paste0("EX_", .convex$exprs)
+    command <- paste0(Jname, " = ", expression_text)
+    ## print(command)
+    .convex$ev$Command(command)
+    structure(Jname, expr = expression_text,
+              command = command,
+              Jname = Jname,
+              proxy = .convex$ev$Eval(Jname),
+              class = "expr")
 }
 
 problem_creator <- function(ptype) {
